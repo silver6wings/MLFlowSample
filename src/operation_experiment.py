de92cec26e38f727c5d8eac6a33f4e9ad4cd18d4
Junchao <silver6wings@gmail.com>
2023-06-01 15:28:43 +0800

    +1

--- src/operation_experiment.py
+++ src/operation_experiment.py
@@ -1,17 +1,21 @@
 import mlflow
 
-# mlflow.create_experiment(
-#     name="test_01",
-#     artifact_location=None,
-#     tags={
-#         "version": "v1",
-#         "priority": "P1",
-#     },
-# )
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+
+mlflow.create_experiment(
+    name="test_02",
+    artifact_location=None,
+    tags={
+        "version": "v1",
+        "priority": "P1",
+    },
+)
 
 experiments = mlflow.search_experiments(
-    filter_string='tags.version="v1"',
+    # filter_string='tags.version="v1"',
 )
 
 for exp in experiments:
+    print(exp.experiment_id)
     print(exp.tags)
+    # mlflow.delete_experiment(experiment_id=exp.experiment_id)
--- src/operation_run.py
+++ src/operation_run.py
@@ -1,3 +1,12 @@
 import mlflow
 
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
 
+experiments = mlflow.search_experiments(
+    filter_string='tags.version="v1"',
+)
+
+for exp in experiments:
+    if exp.name == "test_02":
+     with mlflow.start_run(experiment_id=exp.experiment_id, run_name="abc"):
+        print(exp.tags)
